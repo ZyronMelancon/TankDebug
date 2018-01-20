1fbe46a467dc195749f7a0cabfca50e4e0c1e509
ZyronMelancon <altausolaris@gmail.com>
2018-01-19 18:00:18 -0600

    Init Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float offset;
+    public float smoothSpeed = 0.3f;
+
+    float MouseAim;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    void FixedUpdate()
+    {
+        float rot = target.rotation.eulerAngles.y * Mathf.PI / 180;
+
+        Vector3 pos = target.position + new Vector3(offset * -Mathf.Sin(rot), 3, -offset * Mathf.Cos(rot));
+        Vector3 smoothMove = Vector3.Lerp(transform.position, pos, smoothSpeed);
+        Quaternion smoothRotate = Quaternion.Lerp(transform.rotation, target.rotation, smoothSpeed);
+
+        transform.position = smoothMove;
+        transform.rotation = smoothRotate;
+    }
+}
---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+
+    public float moveForce = 5f;
+    public float hoverForce = 10f;
+    public float hoverHeight = 1f;
+
+    Rigidbody self;
+    private float MouseAim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        self = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MouseAim = Input.GetAxis("Mouse X");
+
+        Move();
+        Hover();
+        Steer();
+
+        Debug.Log(transform.rotation.y);
+        Debug.Log(MouseAim);
+	}
+
+    void Hover()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, hoverHeight))
+        {
+            float dist = (hoverHeight - hit.distance) / hoverHeight;
+            Vector3 force = Vector3.up * dist * hoverForce;
+            self.AddRelativeForce(force, ForceMode.Acceleration);
+        }
+    }
+
+    void Steer()
+    {
+        self.AddRelativeTorque(new Vector3(0, MouseAim, 0));
+    }
+
+    void Move()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        self.AddRelativeForce(dir * moveForce);
+    }
+}
