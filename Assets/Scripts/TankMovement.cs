a708ed6857f18c26d6cdf62bdfbc303446357190
ZyronMelancon <altausolaris@gmail.com>
2018-01-26 18:01:26 -0600

    Overhaul of hover balancing methods. Overall improvement in movement

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -34,94 +34,90 @@ public class TankMovement : MonoBehaviour {
         ray = new Ray(transform.position, -transform.up);
         RaycastHit ground;
 
+        Debug.Log(self.velocity.magnitude);
 
-        //If yes, and not grounded, hover as usual
-        if (Physics.Raycast(ray, out ground, hoverHeight))
+        
+
+        //Will balance to ground slope up to 5 units above ground
+        if (Physics.Raycast(ray, out ground, hoverHeight + 10))
         {
-            if (Input.GetAxis("Jump") == 0)
-            {
-                Hover(ground);
-            }
-        }
-        //If not, try to stay upright until we're over ground again
-        else
-            Rebalance();
-
-        //Can move and steer up to 1 unit above hover height
-        if (Physics.Raycast(ray, out ground, hoverHeight + 1))
-            if (Input.GetAxis("Jump") == 0)
+            HoverBalance();
+            //Can move and steer up to 2 units above hover height
+            if (Input.GetAxis("Jump") <= 0.99f && ground.distance <= hoverHeight + 2)
             {
                 Move();
                 Steer();
+                if(ground.distance <= hoverHeight)
+                    Hover(ground);
             }
+        }
+        else
+            Rebalance();
     }
 
     void Hover(RaycastHit hit)
     {
-        rayT = new Ray(transform.position + offset * 2.5f, -transform.up);
-        rayL = new Ray(transform.position - offset2 * 2.5f, -transform.up);
-        rayR = new Ray(transform.position + offset2 * 2.5f, -transform.up);
-        rayB = new Ray(transform.position - offset * 2.5f, -transform.up);
-
         float dist = (hoverHeight - hit.distance) / hoverHeight;
-        Vector3 force = transform.up * dist * hoverForce;
+        Vector3 force = new Vector3(0, 1 - (self.velocity.y / 8) + ((Mathf.Abs(self.velocity.x)
+            + Mathf.Abs(self.velocity.z)) / 25), 0) * dist * hoverForce * (-Input.GetAxis("Jump") + 1);
 
-        self.AddForce(force, ForceMode.Acceleration);
+        self.AddRelativeForce(force, ForceMode.Acceleration);
+    }
+
+    void HoverBalance()
+    {
+        rayT = new Ray(transform.position + offset * 2f, -transform.up);
+        rayL = new Ray(transform.position - offset2 * 2f, -transform.up);
+        rayR = new Ray(transform.position + offset2 * 2f, -transform.up);
+
+        RaycastHit balT;
+        RaycastHit balL;
+        RaycastHit balR;
 
 
-        /* if (Physics.Raycast(rayT, out hit, hoverHeight))
-         {
-             float dist2 = (hoverHeight - hit.distance);
-             Vector3 force2 = -offset2 * dist2;
-             self.AddRelativeTorque(force2 * 2);
-         }
+        if(Physics.Raycast(rayT, out balT, hoverHeight + 2) 
+        && Physics.Raycast(rayL, out balL, hoverHeight + 2) 
+        && Physics.Raycast(rayR, out balR, hoverHeight + 2))
+        {
+            Vector3 norm = Vector3.Normalize(balT.normal + balL.normal + balR.normal);
 
-         if (Physics.Raycast(rayL, out hit, hoverHeight))
-         {
-             float dist2 = (hoverHeight - hit.distance);
-             Vector3 force2 = -offset * dist2;
-             self.AddRelativeTorque(force2 * 2);
-         }
+            var bal = Quaternion.FromToRotation(transform.up, norm);
+            self.AddTorque(new Vector3(bal.x - 
+                (self.angularVelocity.x / 20), bal.y - 
+                (self.angularVelocity.y / 20), bal.z - 
+                (self.angularVelocity.z / 20)) * 60, 
+                ForceMode.Acceleration);
+        }
 
-         if (Physics.Raycast(rayR, out hit, hoverHeight))
-         {
-             float dist2 = (hoverHeight - hit.distance);
-             Vector3 force2 = offset * dist2;
-             Debug.Log(dist);
-             self.AddRelativeTorque(force2 * 2);
-         }
-
-         if (Physics.Raycast(rayB, out hit, hoverHeight))
-         {
-             float dist2 = (hoverHeight - hit.distance);
-             Vector3 force2 = offset2 * dist2;
-             self.AddRelativeTorque(force2 * 2);
-         }*/
     }
 
     void Steer()
     {
-        var cuck = Quaternion.FromToRotation(transform.forward, camera.transform.forward);
-        Debug.Log(cuck);
-        self.AddTorque(new Vector3(0, Mathf.Min(cuck.y, 0.15f), 0) * 75, ForceMode.Impulse);
+        var steerdir = Quaternion.FromToRotation(transform.forward, camera.transform.forward);
+        self.AddRelativeTorque(new Vector3(0, (steerdir.y - (self.angularVelocity.y / 20)) * (-Input.GetAxis("Jump") + 1), 0) * 75, ForceMode.Impulse);
     }
 
     void Move()
     {
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 dirRel = new Vector3()
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 relDir = Quaternion.AngleAxis(transform.rotation.eulerAngles.y, transform.up) * dir;
 
-        self.AddRelativeForce(dir * moveForce * self.mass);
+        
 
-        if (dir == Vector3.zero)
+        if (dir != Vector3.zero)
+        {
+            self.AddRelativeForce(dir * moveForce * self.mass, ForceMode.Force);
+            self.AddForce(-self.velocity * 7.5f);
+        }
+        else if (dir == Vector3.zero)
             self.AddForce(-self.velocity / 3 * self.mass);
         else
-            self.AddRelativeForce(dir * moveForce * Mathf.Max(0, Vector3.Dot(-self.velocity.normalized, dir)) * self.mass);
+            self.AddRelativeForce(dir * moveForce * Mathf.Max(0, Vector3.Dot(-self.velocity.normalized, relDir)) * self.mass / 2);
     }
 
     void Rebalance()
     {
         var rot = Quaternion.FromToRotation(transform.up, Vector3.up);
-        self.AddTorque(new Vector3(rot.x, rot.y, rot.z) * 60);
+        self.AddTorque(new Vector3(rot.x, rot.y, rot.z) * 200);
     }
 }
